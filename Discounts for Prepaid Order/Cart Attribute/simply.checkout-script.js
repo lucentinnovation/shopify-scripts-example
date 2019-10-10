526983a9dc4623e9b81ed4e408ff925f7a48493a
Ashish <ashish@lucentinnovation.com>
2019-10-10 18:46:25 +0530

    Update simply.checkout-script.js
    Updated with latest code

--- Discounts for Prepaid Order/Cart Attribute/simply.checkout-script.js
+++ Discounts for Prepaid Order/Cart Attribute/simply.checkout-script.js
@@ -6,11 +6,42 @@
  * You can ask Lucent Innovation for any help on website lucentinnovation.com or email shopify@lucentinnovation.com
  * =============================================================================
  */
- 
+
 window.simply = window.simply || {};
-      window.cn = function(o){return"undefined"==typeof o||null==o||""==o.toString().trim()};
-      window.cb = function(o){if(o == 'true'){return true}else{return false}};
-      
+window.cn = function(o){return"undefined"==typeof o||null==o||""==o.toString().trim()};
+window.cb = function(o){if(o == 'true'){return true}else{return false}};
+
+simply.goCheckout = function(){
+  window.location.href = '/checkout';
+};
+
+simply.clickEvent = function(){
+  $(".content-box__row[data-gateway-group != 'manual'] input").click(function(){
+    debugger;
+    if(codAppied){
+      simply.showLoading();
+      $.get('/cart/clear',function(){
+        simply.removeCod();
+      }).fail(function(){  
+        simply.hideLoading();
+      });
+    }
+  });
+  cod_input.click(function(){
+    debugger;
+    if(codAppied){
+    }
+    else{
+      simply.showLoading();
+      $.get('/cart/clear',function(){
+        simply.addCod();
+      }).fail(function(){ 
+        simply.hideLoading();
+      });
+    }
+  });
+};
+
 simply.ajaxCart =  function(form,callback){
   var data = form.serialize();
   var params = {
@@ -32,39 +63,20 @@ simply.ajaxCart =  function(form,callback){
   };
   jQuery.ajax(params);
 };
-simply.ajaxCartRemove =  function(id,callback){
-  var data = {updates: {id: id,quantity:0}};
-  var params = {
-    type: 'POST',
-    url: '/cart/update.js',
-    data: "updates["+id+"]=0",
-    dataType: 'json',
-    success: function(line_item) { 
-      callback();
-    },
-    error: function(XMLHttpRequest, textStatus) {
-      var error=  JSON.parse((XMLHttpRequest.responseText)).description;
-      var error_popup = $("<div id='quick_error'></div>");
-      error_popup.html("");
-      error_popup.append("<h6>"+error+"</h6>");
-      console.log(error_popup);
-      button.text(button.attr("data-text"));
-    }
-  };
-  jQuery.ajax(params);
-};
 
 simply.showLoading = function(){
   $("body").addClass('overflow_hidden');
   $("html").addClass('overflow_hidden');
   $(".loading_me").fadeIn();
 };
+
 simply.hideLoading = function(){
   $("body").removeClass('overflow_hidden');
   $("html").removeClass('overflow_hidden');
   $(".loading_me").hide();
 };
-simply.cartProcess = function(callback){
+
+simply.cartProcess = function(add){
   if(cart_data.length > 0){
     var item = cart_data.pop();
     var form = $("<form>");
@@ -72,6 +84,10 @@ simply.cartProcess = function(callback){
     var qty = $("<input type='hidden' value='"+item.quantity+"' name='quantity'>");
     var prop = item.properties;
     var prop_input;
+    if(add){
+      prop_input =  $("<input type='hidden' value='cod' name='properties[cod]'>");
+      form.append(prop_input);
+    }
     for(var key in prop){
       if(key != 'cod'){
         prop_input =  $("<input type='hidden' value='"+prop[key]+"' name='properties["+key+"]'>");
@@ -81,53 +97,31 @@ simply.cartProcess = function(callback){
 
     form.append(id);
     form.append(qty);
-    scs.ajaxCart(form,simply.cartProcess);
+    if(!cn(add)){
+      simply.ajaxCart(form,simply.addCod);
+    }
+    else{ 
+      simply.ajaxCart(form,simply.removeCod);
+    }
   }
   else{
-    callback();
+    if (Shopify.Checkout.step == "payment_method") {
+      window.location.href = '/checkout';
+    }
+    else{
+      window.location.href = '/checkout?step=shipping_method';
+    }
   }
 };
-simply.goCheckout = function(){
-  window.location.href = '/checkout';
+
+simply.addCod = function(){
+  simply.cartProcess("true");
 };
-simply.removeWithoutCodProduct = function(){
-  simply.ajaxCartRemove(withoutCodProduct,simply.goCheckout);
+
+simply.removeCod = function(){
+  simply.cartProcess();
 };
-simply.addWithoutCodProduct = function(){
-  var form = $("<form>");
-  var id = $("<input name='id' value='"+withoutCodProduct+"'>");
-  form.append(id);
-  simply.ajaxCart(form,simply.goCheckout);
-};
-simply.clickEvent = function(){
-  $(".content-box__row[data-gateway-group != 'manual'] input").click(function(){
-    debugger;
-    if(codAppied){
-      simply.showLoading();
-      if(hasCart){
-        simply.addWithoutCodProduct();
-      }
-      else{
-        simply.cartProcess(simply.addWithoutCodProduct);
-      }
-    }
-  });
-  cod_input.click(function(){
-    debugger;
-    if(codAppied){}
-    else{
-      simply.showLoading();
-      if(hasCart){
-        simply.showLoading();
-        simply.removeWithoutCodProduct();
-      }
-      else
-      {
-        simply.cartProcess(simply.removeWithoutCodProduct);
-      }
-    }
-  });
-};
+
 simply.hoverEvent = function(){};
 simply.changeEvent = function(){};
 simply.submitEvent = function(){};
@@ -138,6 +132,10 @@ simply.init = function(){
   simply.changeEvent();
   simply.submitEvent();
 };
+
 $(document).ready(function(){
   simply.init();
+  $(document).on('page:load page:change', function () {
+
+  });
 });
